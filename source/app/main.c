fec4c9bc2d783e3c62eb12655923fa1b89d08b69
Luc van den Brand <lucvandenbrand@protonmail.com>
2019-01-24 16:00:52 +0100

    Reformatting.

--- source/app/main.c
+++ source/app/main.c
@@ -4,15 +4,16 @@
 
 int main()
 {
-    int result = foo();
-    result += bar(3, 2);
-
     REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
     tte_init_se_default(0, BG_CBB(0)|BG_SBB(31));
     tte_init_con();
 
     tte_printf("Hello, World!\n\n");
+
+    int result = foo();
+    result += bar(3, 2);
     tte_printf("The result = %d.\n\n", result);
+    
     tte_printf("Kind regards,\nLuc van den Brand.");
 
     while(1);
