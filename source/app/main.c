f61f0d8d08e85860421e2adcf03a2ec9c2b3b98e
timostrating <timostrating@home.nl>
2020-04-18 23:59:56 +0200

    added the gba emulator again

--- source/app/main.c
+++ source/app/main.c
@@ -35,12 +35,21 @@ void setupGBA() {
     tte_init_con();
 }
 
+const L1x = 4, L1y = 16;
+const L2x = 0, L2y = 7;
+const L3x = 8, L3y = 9;
+const L4x = 4, L4y = 0;
 
-int main()
-{
+void drawLighting(int x, int y) {
+    m3_line(x + L1x, y + L1y,   x + L2x, y + L2y, CLR_YELLOW);
+    m3_line(x + L2x, y + L2y,   x + L3x, y + L3y, CLR_YELLOW);
+    m3_line(x + L3x, y + L3y,   x + L4x, y + L4y, CLR_YELLOW);
+}
 
-    REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
-    tte_init_se_default(0, BG_CBB(0)|BG_SBB(31));
+int main() {
+
+    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+    tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
 
     tte_write("#{P:62,64}");
     tte_write("Save the GameBoy!");
@@ -48,21 +57,25 @@ int main()
     tte_write("#{P:10,140}");
     tte_write("Press the <any> key to start");
 
-    while(!KEY_DOWN_NOW(KEY_ANY));
+    while (!KEY_DOWN_NOW(KEY_ANY));
 
 
     int ii, jj;
     setupGBA();
 
-    // Fill screen with grey color
     m3_fill(CLR_GRAY);
 
-    // Lines in bottom left frame
-    for(ii=0; ii<=10; ii++)
-    {
-        jj= 3*ii+7;
-        m3_line(24*ii, 0, 240-24*ii, 160, RGB15(0, jj, jj));
+    int x=0, y=0;
+    while (true) {
+        key_poll();
+
+        drawLighting(x, y);
+        for (ii = 0; ii <= 10; ii++) {
+            jj = 3 * ii + 7;
+            m3_line(24 * ii, 0, 240 - 24 * ii, 160, RGB15(0, jj, jj));
+        }
     }
+
 
     while(1);
     return 0;
