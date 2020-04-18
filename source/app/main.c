303c71ae45902a9ae37daa6da86a0843f3e87a9f
timostrating <timostrating@home.nl>
2020-04-19 01:56:44 +0200

    game loop

--- source/app/main.c
+++ source/app/main.c
@@ -35,10 +35,11 @@ void setupGBA() {
     tte_init_con();
 }
 
-const L1x = 4, L1y = 16;
-const L2x = 0, L2y = 7;
-const L3x = 8, L3y = 9;
-const L4x = 4, L4y = 0;
+
+const int L1x = 4, L1y = 16;
+const int L2x = 0, L2y = 7;
+const int L3x = 8, L3y = 9;
+const int L4x = 4, L4y = 0;
 
 void drawLighting(int x, int y) {
     m3_line(x + L1x, y + L1y,   x + L2x, y + L2y, CLR_YELLOW);
@@ -46,36 +47,46 @@ void drawLighting(int x, int y) {
     m3_line(x + L3x, y + L3y,   x + L4x, y + L4y, CLR_YELLOW);
 }
 
+const int CLR_BACKGROUND = 0x0000;
+const int CLR_LINE = 0x1111;
+const int CLR_ENERGY = 0x2222;
+
+void playLevel() {
+
+    int x=0, y=20;
+    while (true) {
+        vid_vsync();
+        key_poll();
+        m3_fill(CLR_BACKGROUND);
+
+        m3_frame(20, 20, 240-20, 160-20, CLR_LINE);
+
+        m3_vline(100, 20, 160-20, CLR_LINE);
+        m3_vline(140, 20, 160-20, CLR_LINE);
+        m3_hline(20, 80, 240-20, CLR_LINE); // hline x1, y, x2
+
+        m3_rect(x-1,y-1, x+1,y+1, CLR_ENERGY);
+        x++;
+        x %= 240;
+    }
+}
+
 int main() {
 
-    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
-    tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
+//    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+//    tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
+//
+//    tte_write("#{P:62,64}");
+//    tte_write("Save the GameBoy!");
+//
+//    tte_write("#{P:10,140}");
+//    tte_write("Press the <any> key to start");
+//
+//    while (!KEY_DOWN_NOW(KEY_ANY));
 
-    tte_write("#{P:62,64}");
-    tte_write("Save the GameBoy!");
 
-    tte_write("#{P:10,140}");
-    tte_write("Press the <any> key to start");
-
-    while (!KEY_DOWN_NOW(KEY_ANY));
-
-
-    int ii, jj;
     setupGBA();
-
-    m3_fill(CLR_GRAY);
-
-    int x=0, y=0;
-    while (true) {
-        key_poll();
-
-        drawLighting(x, y);
-        for (ii = 0; ii <= 10; ii++) {
-            jj = 3 * ii + 7;
-            m3_line(24 * ii, 0, 240 - 24 * ii, 160, RGB15(0, jj, jj));
-        }
-    }
-
+    playLevel();
 
     while(1);
     return 0;
