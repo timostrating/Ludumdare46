f006f603cfa0a5094828f1bdb17a3e7ff95ee539
timostrating <timostrating@home.nl>
2020-04-18 18:44:38 +0200

    + clion cmake support + simple mode switch

--- source/app/main.c
+++ source/app/main.c
@@ -4,46 +4,66 @@
 #include "audio.h"
 #include "gbfs.h"
 
-Audio createEmptyAudio() {
-    Audio audio;
-    audio.numChannels = 0;
-    return audio;
+// Audio createEmptyAudio() {
+//     Audio audio;
+//     audio.numChannels = 0;
+//     return audio;
+// }
+
+// Audio loadAudio() {
+//     u32 audioDataSize = 0;
+//     const GBFS_FILE * audioFile = find_first_gbfs_file(find_first_gbfs_file);
+//     const u16 * audioData = gbfs_get_obj(audioFile, "Test00.bin", &audioDataSize);
+
+//     if (audioData == NULL) {
+//         tte_printf("No GBFS audio found.\n");
+//         return createEmptyAudio();
+//     }
+
+//     return loadAudioFromROM(audioData);
+// }
+
+void setupGBA() {
+    // set Display mode
+    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+
+    // setup interrupts
+    irq_init(NULL);
+
+    // setupTextMode
+    tte_init_se_default(3, BG_CBB(0) | BG_SBB(31));
+    tte_init_con();
 }
 
-Audio loadAudio() {
-    u32 audioDataSize = 0;
-    const GBFS_FILE * audioFile = find_first_gbfs_file(find_first_gbfs_file);
-    const u16 * audioData = gbfs_get_obj(audioFile, "Test00.bin", &audioDataSize);
-
-    if (audioData == NULL) {
-        tte_printf("No GBFS audio found.\n");
-        return createEmptyAudio();
-    }
-
-    return loadAudioFromROM(audioData);
-}
 
 int main()
 {
+
     REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
     tte_init_se_default(0, BG_CBB(0)|BG_SBB(31));
-    tte_init_con();
 
-    initAudioSystem();
-    irq_init(NULL);
-    irq_add(II_VBLANK, tickAudioSystem);
+    tte_write("#{P:62,64}");
+    tte_write("Save the GameBoy!");
 
-    tte_printf("Hello, World!\n\n");
+    tte_write("#{P:10,140}");
+    tte_write("Press the <any> key to start");
 
-    int result = foo();
-    result += bar(3, 2);
-    tte_printf("The result = %d.\n", result);
+    while(!KEY_DOWN_NOW(KEY_ANY));
 
-    tte_printf("Now playing audio.\n");
-    Audio audio = loadAudio();
-    setCurrentAudio(&audio);
 
-    tte_printf("\nKind regards,\nLuc van den Brand.");
+    int ii, jj;
+    setupGBA();
+
+    // Fill screen with grey color
+    m3_fill(CLR_GRAY);
+
+    // Lines in bottom left frame
+    for(ii=0; ii<=10; ii++)
+    {
+        jj= 3*ii+7;
+        m3_line(24*ii, 0, 240-24*ii, 160, RGB15(0, jj, jj));
+    }
 
     while(1);
+    return 0;
 }
