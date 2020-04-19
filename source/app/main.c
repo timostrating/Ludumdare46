6cd8aa98afaf605b42312923bf6371180e3428ad
timostrating <timostrating@home.nl>
2020-04-19 15:25:13 +0200

    multiple levels on the game boyyyyy

--- source/app/main.c
+++ source/app/main.c
@@ -46,10 +46,11 @@ void drawLighting(int x, int y) {
     m3_line(x + L2x, y + L2y,   x + L3x, y + L3y, CLR_YELLOW);
     m3_line(x + L3x, y + L3y,   x + L4x, y + L4y, CLR_YELLOW);
 }
-typedef struct Level {
-    Energy energies;
-    Gate gates;
-} Level;
+
+typedef struct Line {
+    u8 x1, y1;
+    u8 x2, y2;
+} Line;
 
 typedef struct Energy {
     u8 index;
@@ -62,21 +63,55 @@ typedef struct Gate {
     bool horizontal;
 } Gate;
 
+typedef struct Level {
+    u8 nrLs; Line ls[10];
+    u8 nrGs; Gate gs[10];
+    u8 nrEs; Energy es[10];
+} Level;
 
-Energy e = (Energy){.index=0, .x=20, .y=20, .pos={220,20, 220,80, 20,80} };
-Gate g = (Gate){.x=120, .y=80, .horizontal=true};
 
-bool playLevel(u8 level) {
+Level levels[] = {
+        {
+            .nrLs=1, .ls={(Line){.x1=20, .y1=80, .x2=240 - 20, .y2=80}},
+            .nrGs=1, .gs={(Gate){.x=120, .y=80, .horizontal=true}},
+            .nrEs=2, .es={
+                (Energy){.index=0, .x=220, .y=20, .pos={20,20, 20,140, 220,140, 220,20, 20,20} },
+                (Energy){.index=0, .x=20, .y=20, .pos={220,20, 220,80, 20,80} }
+            },
+        },
+        {
+            .nrLs=1, .ls={(Line){.x1=20, .y1=80, .x2=240 - 20, .y2=80}},
+            .nrGs=1, .gs={(Gate){.x=80, .y=120, .horizontal=true}},
+            .nrEs=3, .es={
+                (Energy){.index=0, .x=220, .y=20, .pos={20,20, 20,140, 220,140, 220,20, 20,20} },
+                (Energy){.index=0, .x=20, .y=20, .pos={20,20, 20,140, 220,140, 220,20, 20,20} },
+                (Energy){.index=0, .x=20, .y=20, .pos={220,20, 220,80, 20,80} }
+            },
+        },
+        {
+            .nrLs=1, .ls={(Line){.x1=20, .y1=80, .x2=240 - 20, .y2=80}},
+            .nrGs=1, .gs={(Gate){.x=80, .y=120, .horizontal=true}},
+            .nrEs=3, .es={
+                (Energy){.index=0, .x=220, .y=20, .pos={20,20, 20,140, 220,140, 220,20, 20,20} },
+                (Energy){.index=0, .x=20, .y=20, .pos={20,20, 20,140, 220,140, 220,20, 20,20} },
+                (Energy){.index=0, .x=20, .y=20, .pos={220,20, 220,80, 20,80} }
+            },
+        }
+};
+
+bool playLevel(u8 lvl) {
 
     int CLR_BACKGROUND = RGB8(31, 119, 77);
     int CLR_LINE = RGB8(155, 199, 84);
     int CLR_ENERGY = RGB8(50, 50, 180);
     int CLR_GATE = RGB8(50, 50, 180);
 
-    bool flipState = true;
+    bool horizontalState = true;
 
     int frame=0;
     int inputFrame=0;
+    int gates = levels[lvl].nrGs;
+
     while (true) {
         vid_vsync();
 
@@ -84,30 +119,47 @@ bool playLevel(u8 level) {
 
         if (key_tri_horz() != 0 && frame - inputFrame > 10) {
             inputFrame = frame;
-            flipState = !flipState;
+            horizontalState = !horizontalState;
         }
 
         m3_fill(CLR_BACKGROUND);
 
         m3_frame(20, 20, 240-20, 160-20, CLR_LINE);
 
-//        m3_vline(100, 20, 160-20, CLR_LINE);
-//        m3_vline(140, 20, 160-20, CLR_LINE);
-        m3_hline(20, 80, 240-20, CLR_LINE); // hline x1, y, x2
+        // Lines
+        Line* l = &(levels[lvl].ls[0]);
+        for (int i=0; i<levels[lvl].nrLs; i++) {
+            l = &(levels[lvl].ls[i]);
+            m3_line(l->x1, l->y1, l->x2, l->y2, CLR_LINE);
+        }
 
 
-        // gates
-        m3_rect(g.x-5,g.y-5, g.x+5,g.y+5, CLR_BACKGROUND);
-        if (g.horizontal == flipState) { m3_rect(g.x-5,g.y-1, g.x+5,g.y+1, CLR_ENERGY); }
-        else                           { m3_rect(g.x-1,g.y-5, g.x+1,g.y+5, CLR_ENERGY); }
+        // Gates
+        Gate* g = &(levels[lvl].gs[0]);
+        for (int i=0; i<levels[lvl].nrGs; i++) {
+            g = &(levels[lvl].gs[i]);
+            m3_rect(g->x-5,g->y-5, g->x+5,g->y+5, CLR_BACKGROUND);
+            if (g->horizontal == horizontalState) { m3_rect(g->x-5,g->y-1, g->x+5,g->y+1, CLR_ENERGY); }
+            else                                  { m3_rect(g->x-1,g->y-5, g->x+1,g->y+5, CLR_ENERGY); }
+        }
 
 
         // Energies
-        m3_rect(e.x-2,e.y-2, e.x+2,e.y+2, CLR_ENERGY);
+        Energy* e = &(levels[lvl].es[0]);
+        for (int i=0; i<levels[lvl].nrEs; i++) {
+            e = &(levels[lvl].es[i]);
 
-        if (e.x == e.pos[e.index*2] && e.y == e.pos[e.index*2+1]) {e.index += 1;}
-        e.x = (e.x < e.pos[e.index*2  ])? ++e.x : ((e.x == e.pos[e.index*2  ])? e.x : --e.x);
-        e.y = (e.y < e.pos[e.index*2+1])? ++e.y : ((e.y == e.pos[e.index*2+1])? e.y : --e.y);
+            m3_rect(e->x-2,e->y-2, e->x+2,e->y+2, CLR_ENERGY);
+
+            if (e->x == e->pos[e->index*2] && e->y == e->pos[e->index*2+1]) {e->index += 1;}
+            e->x = (e->x < e->pos[e->index*2  ])? ++e->x : ((e->x == e->pos[e->index*2  ])? e->x : --e->x);
+            e->y = (e->y < e->pos[e->index*2+1])? ++e->y : ((e->y == e->pos[e->index*2+1])? e->y : --e->y);
+
+            if (e->x == g->x && e->y == g->y) {
+                if ((g->horizontal == horizontalState) != (e->y == e->pos[e->index * 2 + 1])) { return false; }
+                else if (--gates <= 0) { return true; }
+            }
+        }
 
         frame++;
     }
@@ -130,20 +182,23 @@ int main() {
 
 
     setupGBA();
-    int level = 1;
+    int level = 0;
     while (true) {
+        setupGBA();
         bool hasWon = playLevel(level);
+        level++;
+
         if (hasWon == false) {
             break;
         }
     }
 
-//    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
-//    tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
-//    vid_vsync();
-//
-//    tte_write("#{P:62,64}");
-//    tte_write("You Lose!");
+    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+    tte_init_se_default(0, BG_CBB(0) | BG_SBB(31));
+    vid_vsync();
+
+    tte_write("#{P:72,64}");
+    tte_write("You Lose!");
 
 
 
